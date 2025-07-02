b3b6280a6a85c14e2355addb0b4895fbf23136c5
Ahasanul Arafath <ahasanular@gmail.com>
2025-07-02 22:45:05 +0600

    [v1.0.3] converted static tailwind cdn to local package

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Ensure this is pointing to your static folder
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
